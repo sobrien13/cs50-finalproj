f40a338261bfa0ed3771b39f23e8aaa499e50d69
Alex Underwood <alexunderwood@aunderwood-mba.rsknet>
2019-05-08 12:55:32 -0700

    added attack options

---
+++ battle_test.py
@@ -0,0 +1,42 @@
+#!/bin/us/python3
+
+# import appropriate libraries
+import random
+import sqlite3
+
+# pre-battle setup
+# import DB and player stat table
+connection = sqlite3.connect("final.db") # insert correct DB name
+cVar = connection.cursor()
+player_health = cVar.execute("select health from player_tbl") # insert correct table name
+
+
+
+# create enemy health and damage
+def enemyStats():
+	enemy_health = player_health * (random.randint(1, 6) * 1.15)
+	enemy_dmg = random.randint(1, 5) * player_health
+	cVar.execute("create table enemy (health, damage);")
+	enemybuild = cVar.execute("insert into enemy values (?, ?)", (enemy_health, enemy_dmg))
+	
+	connection.commit()
+
+enemyStats()
+
+#list of possible enemies
+List = ["Golem", "Troll", "Orc", "Ghost", "Mummy", "Goblin"]
+
+
+def battle():
+	print("You have now entered a battle!")
+	e = random.randint(1, 6)
+	print("From the shadows emerges a", List[e])
+	sql_skills = cVar.execute("select attack from skills") # skill table needed
+	result = cVar.fetchall()
+	attackList = []
+        for i in result:
+            print(i)
+        	attackList.append[i]
+    attack = input("What will you do?: ")
+
+	connection.commit()
